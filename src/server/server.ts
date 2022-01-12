6fe8b99fa5947a133975496a4d06ecffcccfa7c5
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-01-12 09:58:41 +0000

    docs: add files needed for deployment and views
    Concourse job failed last night. Add files that are required for successful deployment and additional (i.e. 500) views etc.
    
    Reviewed-by: James, Sam & Rich
    Refs: BLAIS5-2981

---
+++ src/server/server.ts
@@ -0,0 +1,30 @@
+import express, {Request, Response} from "express";
+import path from "path";
+import ejs from "ejs";
+import HealthCheckHandler from "./handlers/healthCheckHandler";
+
+function NewServer(): any {
+    const server = express();
+    const buildFolder = "../build";
+    server.set("views", path.join(__dirname, buildFolder));
+    server.engine("html", ejs.renderFile);
+    server.use("/static", express.static(path.join(__dirname, `${buildFolder}/static`)));
+
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
+
+    //define handlers
+    server.use("/", HealthCheckHandler());
+
+    //define entry point
+    server.get("*", function (req: Request, res: Response) {
+        res.render("index.html");
+    });
+
+    server.use(function (err: Error, req: Request, res: Response) {
+        res.render("../src/views/500.html", {});
+    });
+    return server;
+}
+
+export default NewServer;
