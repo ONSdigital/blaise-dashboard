f12155a817cdc5c76c24e35188946a170c0885cd
Sam Bryant <srbry@hotmail.com>
2022-01-21 11:39:15 +0000

    Feat: Add auto refresh

---
+++ src/client/refreshInterval.ts
@@ -0,0 +1,1 @@
+export const refreshInterval = 60 * 1000; // 60 seconds
--- src/components/instrumentCaseReportTable.tsx
+++ src/components/instrumentCaseReportTable.tsx
@@ -1,9 +1,10 @@
-import React, {Component, ReactElement} from "react";
-import {CaseCompletionReport} from "../server/blaiseApi/caseCompletionReport";
-import {getCaseCompletionReport} from "../client/caseCompletionReport";
-import {Instrument} from "blaise-api-node-client";
-import {ONSLoadingPanel, ONSPanel, ONSTable} from "blaise-design-system-react-components";
+import React, { Component, ReactElement } from "react";
+import { CaseCompletionReport } from "../server/blaiseApi/caseCompletionReport";
+import { getCaseCompletionReport } from "../client/caseCompletionReport";
+import { Instrument } from "blaise-api-node-client";
+import { ONSLoadingPanel, ONSPanel, ONSTable } from "blaise-design-system-react-components";
 import InstrumentCaseReport from "./instrumentCaseReport";
+import { refreshInterval } from "../client/refreshInterval";
 
 type InstrumentCaseReportTableProps = {
     instruments: Instrument[]
@@ -16,6 +17,8 @@ type InstrumentCaseReportTableState = {
 }
 
 export default class InstrumentCaseReportTable extends Component<InstrumentCaseReportTableProps, InstrumentCaseReportTableState> {
+    interval!: ReturnType<typeof setInterval>;
+
     constructor(props: InstrumentCaseReportTableProps) {
         super(props);
         this.state = {
@@ -27,46 +30,58 @@ export default class InstrumentCaseReportTable extends Component<InstrumentCaseR
 
     componentDidMount() {
         console.log("Getting case completion reports for mount");
+        this.loadReports();
+        this.interval = setInterval(() => {
+            this.loadReports();
+        }, refreshInterval);
+        console.log("Got case completion reports for mount");
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    loadReports(): void {
         this.getCaseCompletionReports().then((caseCompletionReports: Record<string, CaseCompletionReport>) => {
             this.setState({
                 loaded: true,
                 caseCompletionReports: caseCompletionReports
             });
-            console.log("Got case completion reports for mount");
         });
     }
 
     async getCaseCompletionReports(): Promise<Record<string, CaseCompletionReport>> {
-        if (!this.state.loaded) {
-            const caseCompletionReports: Record<string, CaseCompletionReport> = {};
-            for (const instrument of this.props.instruments) {
-                console.log(`Getting completion report for ${instrument.name}`);
-                try {
-                    caseCompletionReports[instrument.name] = await getCaseCompletionReport(instrument.name);
-                } catch (reason: any) {
-                    console.error(`Error getting case completion report for ${instrument.name}: ${reason}`);
-                    const erroredInstruments = this.state.erroredInstruments;
-                    erroredInstruments.push(instrument.name);
-                    this.setState({
-                        erroredInstruments: [...new Set(erroredInstruments)]
-                    });
-                }
+        const caseCompletionReports: Record<string, CaseCompletionReport> = {};
+        for (const instrument of this.props.instruments) {
+            console.log(`Getting completion report for ${instrument.name}`);
+            try {
+                caseCompletionReports[instrument.name] = await getCaseCompletionReport(instrument.name);
+            } catch (reason: unknown) {
+                console.error(`Error getting case completion report for ${instrument.name}: ${reason}`);
+                const erroredInstruments = this.state.erroredInstruments;
+                erroredInstruments.push(instrument.name);
+                this.setState({
+                    erroredInstruments: [...new Set(erroredInstruments)]
+                });
             }
-            return caseCompletionReports;
         }
-        return {};
+        return caseCompletionReports;
     }
 
     errorPanel(): ReactElement | undefined {
         if (this.state.erroredInstruments.length === 0) {
             return undefined;
         }
-        return <ONSPanel status="error">Failed to get completion reports for instruments: {this.state.erroredInstruments}</ONSPanel>;
+        return (
+            <ONSPanel status="error">
+                Failed to get completion reports for instruments: {this.state.erroredInstruments.join(", ")}
+            </ONSPanel>
+        );
     }
 
     render(): ReactElement {
         if (!this.state.loaded) {
-            return <ONSLoadingPanel message={"Getting case completion reports"}/>;
+            return <ONSLoadingPanel message={"Getting case completion reports"} />;
         }
 
         const caseReportRows: ReactElement[] = [];
