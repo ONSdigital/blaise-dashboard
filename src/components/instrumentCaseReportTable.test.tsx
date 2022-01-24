82f51e2575ac58d7d2bdbd47ba9af9cd0d081977
Sam Bryant <srbry@hotmail.com>
2022-01-24 09:59:53 +0000

    Add tests for front end edge cases

--- src/components/instrumentCaseReportTable.test.tsx
+++ src/components/instrumentCaseReportTable.test.tsx
@@ -21,12 +21,19 @@ describe("InstrumentCaseReportTable", () => {
         NotComplete: 2,
         CompletePercentage: 71.43
     };
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
     it("renders a table row for the instrument case status", async () => {
         getCaseCompletionReportMock.mockImplementation(() => Promise.resolve(caseCompletionReport));
 
         render(
             <InstrumentCaseReportTable instruments={mockInstrumentList} />
         );
+
+        expect(screen.queryByText(/Loading/i)).toBeDefined();
 
         await waitFor(() => {
             expect(screen.getByTestId("instrument-case-report-questionnaire-OPN2101A").textContent).toEqual("OPN2101A");
@@ -36,6 +43,20 @@ describe("InstrumentCaseReportTable", () => {
                 "<progress id=\"file\" value=\"71.43\" max=\"100\" role=\"progressbar\" aria-valuenow=\"71.43\" aria-valuemin=\"0\" aria-valuemax=\"100\">71.43%</progress>"
             );
             expect(screen.getByTestId("case-report-table")).toMatchSnapshot();
+        });
+    });
+
+    describe("when getting case completion reports errors", () => {
+        it("renders an error panel", async () => {
+            getCaseCompletionReportMock.mockImplementation(() => Promise.reject("Cannot get case completion reports"));
+
+            render(
+                <InstrumentCaseReportTable instruments={mockInstrumentList} />
+            );
+
+            await waitFor(() => {
+                expect(screen.getByText("Failed to get completion reports for instruments: LMS2101_CC1, OPN2101A, OPN2007T")).toBeDefined();
+            });
         });
     });
 });
