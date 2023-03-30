e1ee1c3f47c5427682eaa7a89c691efacd002142
SidraJaved <sidrajaved92@gmail.com>
2023-03-30 14:57:29 +0100

    test: test case for google monitoring api changes
    Refs: BLAIS5-3416

--- src/server/googleCloudMonitoringApi/monitoring.test.ts
+++ src/server/googleCloudMonitoringApi/monitoring.test.ts
@@ -8,21 +8,19 @@ import { google } from "@google-cloud/monitoring/build/protos/protos";
 type GetUptimeChecksConfigsResult = [google.monitoring.v3.IUptimeCheckConfig[], google.monitoring.v3.IListUptimeCheckConfigsRequest | null, google.monitoring.v3.IListUptimeCheckConfigsResponse];
 
 jest.spyOn(monitoring , "getUptimeChecksConfigs").mockReturnValue(Promise.resolve( [[], null, []] as GetUptimeChecksConfigsResult));
-jest.spyOn(monitoring , "listTimeSeries").mockReturnValue(Promise.resolve( [[], null, []]  ) as ReturnType <typeof monitoring.listTimeSeries> );
+//jest.spyOn(monitoring , "listTimeSeries").mockReturnValue(Promise.resolve( [[], null, []]  ) as ReturnType <typeof monitoring.listTimeSeries> );
+
+import { getUptimeChecksConfigs } from "./monitoring";
+const getUptimeChecksConfigsMock = getUptimeChecksConfigs as unknown as jest.Mock<Promise<GetUptimeChecksConfigsResult[]>>;
 
 
 describe("Get all uptime checks from API", () => {
     
     it("should return a 200 status and a json list of 1 items when API returns a 1 item list", async () => {
-        getMonitoringUptimeCheckTimeSeries("anything");
+        getUptimeChecksConfigsMock.mockImplementation(() => Promise.resolve([]));
+        const response = await getMonitoringUptimeCheckTimeSeries("ons-blaise-v2-dev-sj02");
+        expect(response).toStrictEqual([{"hostname":"unknown","regions":[{"region":"unknown", "status":"false"}]}]);
     });
-
-    // it("should return a 500 status direct from the API", async () => {
-    //     getMonitoringUptimeCheckTimeSeriesMock.mockImplementation(() => Promise.reject("Error getting uptime checks"));
-    //     const response = await request.get("/api/monitoring");
-    //     expect(response.status).toEqual(500); 
-    //     expect(response.body).toEqual("Failed to get monitoring uptimeChecks config data");
-    // });
 
     afterEach(() => {
         jest.clearAllMocks();
