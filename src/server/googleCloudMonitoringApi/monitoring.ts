bfd1c5785a6ec70995192c8a25b829b11628aae0
SidraJaved <sidrajaved92@gmail.com>
2023-03-31 11:51:34 +0100

    test: unit tests for monitoring.ts to test that google api is correctly returning the responses when right arguments are passed
    refactoring: cleaned up the code to make it more readable and understandable
    
    Refs: BLAIS5-3416

--- src/server/googleCloudMonitoringApi/monitoring.ts
+++ src/server/googleCloudMonitoringApi/monitoring.ts
@@ -1,105 +1,65 @@
-import monitoring, { MetricServiceClient, UptimeCheckServiceClient } from "@google-cloud/monitoring";
-import { MonitoringDataModel, Region} from "../monitoringDataModel";
-import { google } from "@google-cloud/monitoring/build/protos/protos";
+import {MonitoringDataModel, Region} from "../monitoringDataModel";
+import {google} from "@google-cloud/monitoring/build/protos/protos";
+import * as protos from "@google-cloud/monitoring/build/protos/protos";
 
-const uptimeclient = new monitoring.UptimeCheckServiceClient();
-const metricsClient = new monitoring.MetricServiceClient();
+const regionsMonitored: string [] = ["eur-belgium", "apac-singapore", "usa-oregon", "sa-brazil-sao_paulo"];
 
-const regionsMonitored : string [] = ["eur-belgium", "apac-singapore", "usa-oregon","sa-brazil-sao_paulo"];
+export type GetUptimeChecksConfigResult = protos.google.monitoring.v3.IUptimeCheckConfig[];
+export type ListTimeSeriesResult = protos.google.monitoring.v3.ITimeSeries[];
 
-export async function getMonitoringUptimeCheckTimeSeries(
-    projectId: string
-): Promise<MonitoringDataModel[]> {
-    
-    try 
-    {
-        const [uptimeCheckConfigs] = await getUptimeChecksConfigs(projectId);
-        const monitoringDataResponse = uptimeCheckConfigs.map((uptimeCheckConfig) => fetchHostnames(uptimeCheckConfig, projectId));
-
-        return await Promise.all(monitoringDataResponse);
-    } catch (error: any) {
-       // console.error(`Response: ${error}`);
-        return [{"hostname":"unknown","regions":[{"region":"unknown", "status":"false"}]}];
-    }
+export interface GoogleMonitoring {
+    getUptimeChecksConfigs(): Promise<GetUptimeChecksConfigResult>;
+    listTimeSeries(filter: string, startTime: number, endTime: number): Promise<ListTimeSeriesResult>;
 }
 
-    async function fetchHostnames ( uptimeCheckConfig:google.monitoring.v3.IUptimeCheckConfig, projectId: string) {
-        const hostname = uptimeCheckConfig?.monitoredResource?.labels?.host!;
-        const regions = regionsMonitored.map((region) => fetchTimeSeriesPoints(region, hostname, projectId));
-        const monitoringDataObject : MonitoringDataModel = {
+export async function getMonitoringUptimeCheckTimeSeries(
+    
+        googleMonitoring: GoogleMonitoring
+
+    ): Promise<MonitoringDataModel[]> {
+
+    const nowInSeconds = Date.now() / 1000;
+    const startTime = nowInSeconds - 30;
+    const endTime = nowInSeconds;
+
+    try {
+        const uptimeCheckConfigs = await googleMonitoring.getUptimeChecksConfigs();
+        const monitoringDataResponse = uptimeCheckConfigs.map(fetchHostnames);
+        return Promise.all(monitoringDataResponse);
+    } catch (error: any) {
+        console.error(`Response: ${error}`);
+        return [{"hostname": "unknown", "regions": [{"region": "unknown", "status": "false"}]}];
+    }
+
+
+    async function fetchHostnames(uptimeCheckConfig: google.monitoring.v3.IUptimeCheckConfig): Promise<MonitoringDataModel> {
+        const hostname = uptimeCheckConfig.monitoredResource?.labels?.host!;
+        const regions = regionsMonitored.map((region) => fetchTimeSeriesPoints(region, hostname));
+        return {
             hostname: hostname,
             regions: await Promise.all(regions)
         };
-        return monitoringDataObject;
-    };
 
-//  var fetchHostnames = function getData(projectId: string){
-//     return async function(uptimeCheckConfig:google.monitoring.v3.IUptimeCheckConfig) 
-//     {
-//         const hostname = uptimeCheckConfig?.monitoredResource?.labels?.host!;
-//         const regions = regionsMonitored.map((region) => fetchTimeSeriesPoints(region, hostname, projectId));
-//         const monitoringDataObject : MonitoringDataModel = {
-//             hostname: hostname,
-//             regions: await Promise.all(regions)
-//         };
-//         return monitoringDataObject;
-//     };
-
-// };
-
- async function fetchTimeSeriesPoints ( regionMonitored : string, hostname : string, projectId: string) {
-    //get timeSeries points
-    try{
-        const filter = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" resource.label.\"host\"=\"" + hostname + "\" metric.label.\"checker_location\"=\"" + regionMonitored+ "\"";
-        const startTime = Date.now() / 1000 - 60 * 1/2;
-        const endTime = Date.now() / 1000;
-        
-        const [timeSeries] = await listTimeSeries(filter, startTime, endTime, projectId);
-
-        console.log("Status: for "+ hostname + " in region = "+ regionMonitored + " is " + JSON.stringify( timeSeries[0].points?.at(0)?.value?.boolValue));
-        var region : Region = {
-            region : regionMonitored,
-            status :  timeSeries[0].points?.at(0)?.value?.boolValue == true ? "success" : "error" 
-        };
-        return region;
-    }
-    catch (error: any) {
-        console.error(`Response: ${error}`);
-        console.log("Failed to get timeSeries points data");
-       var region : Region = {
-        region : regionMonitored,
-        status : "requestFailed"
-        };
-        return region;
-    }
-}
-
-//Google calling functions -- cannot be testes :) 
-
-export async function getUptimeChecksConfigs (projectId: string){
-
-    const request = {
-        parent: uptimeclient.projectPath(projectId)
-    };
-
-    // Retrieves an uptime check config with hostnames-services like dqs, bus, bum, dashboard etc
-    return uptimeclient.listUptimeCheckConfigs(request);
-}
-
-export async function listTimeSeries (filter : string,startTime: number , endTime : number, projectId : string ){
-    const request = {
-        name: metricsClient.projectPath(projectId),
-        filter: filter,
-        interval: {
-            startTime: {
-                // Limit results to the last 30 seconds
-                seconds: startTime,
-            },
-            endTime: {
-                seconds: endTime
+        async function fetchTimeSeriesPoints(regionMonitored: string, hostname: string): Promise<Region> {
+            const filter = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" " +
+                `resource.label."host"="${hostname}" metric.label."checker_location"="${regionMonitored}"`;
+            const status = await listTimeSeries(filter, hostname, regionMonitored);
+    
+            return {
+                region: regionMonitored,
+                status: status
+            };
+        }
+    
+        async function listTimeSeries(filter: string, hostname: string, regionMonitored: string): Promise<string> {
+            try {
+                const timeSeries = await googleMonitoring.listTimeSeries(filter, startTime, endTime);
+                return timeSeries[0].points?.at(0)?.value?.boolValue == true ? "success" : "error";
+            } catch (error: any) {
+                console.error(`Response: ${error}`);
+                console.log("Failed to get timeSeries points data");
+                return "requestFailed";
             }
         }
-    };
-    // Writes time series data
-    return metricsClient.listTimeSeries(request);
-}
+    }
+    }
